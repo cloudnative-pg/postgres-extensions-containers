b150f32a213e66a2fa494a41619d2d7381dad3f9
Niccolò Fei <niccolo.fei@enterprisedb.com>
2026-02-05 12:16:57 +0100

    feat: generate extension catalogs (#93)
    Add a new dagger command to generate extensions catalog (e.g
    ClusterImageCatalogs with extensions) starting from a base set of
    `minimal` catalogs, like the ones currently in
    https://github.com/cloudnative-pg/artifacts/tree/main/image-catalogs.
    
    Add a new GH workflow that, at the end of every successful build on
    `main`, triggers an update of the catalogs which will be uploaded
    inside the `image-catalogs` directory of
    [cloudnative-pg/artifacts](https://github.com/cloudnative-pg/artifacts).
    
    Closes cloudnative-pg/artifacts#7

--- dagger/maintenance/image.go
+++ dagger/maintenance/image.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strconv"
 
+	"github.com/google/go-containerregistry/pkg/crane"
 	"github.com/google/go-containerregistry/pkg/name"
 	containerregistryv1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
@@ -16,6 +17,11 @@ const (
 	DefaultPgMajor      = 18
 	DefaultDistribution = "trixie"
 )
+
+var SupportedDistributions = []string{
+	"bookworm",
+	"trixie",
+}
 
 // getImageAnnotations returns the OCI annotations given an image ref.
 func getImageAnnotations(imageRef string) (map[string]string, error) {
@@ -51,10 +57,79 @@ func getImageAnnotations(imageRef string) (map[string]string, error) {
 // getDefaultExtensionImage returns the default extension image for a given extension,
 // resolved from the metadata.
 func getDefaultExtensionImage(metadata *extensionMetadata) (string, error) {
-	packageVersion := metadata.Versions[DefaultDistribution][strconv.Itoa(DefaultPgMajor)]
+	return getExtensionImage(metadata, DefaultDistribution, DefaultPgMajor)
+}
+
+// getExtensionImage returns the extension image for a given distribution and pgMajor.
+func getExtensionImage(metadata *extensionMetadata, distribution string, pgMajor int) (string, error) {
+	version, err := extractExtensionVersion(metadata.Versions, distribution, pgMajor)
+	if err != nil {
+		return "", fmt.Errorf("while extracting extension version for %s: %w", metadata.Name, err)
+	}
+
+	image := fmt.Sprintf("ghcr.io/cloudnative-pg/%s:%s-%d-%s",
+		metadata.ImageName, version, pgMajor, distribution)
+
+	return image, nil
+}
+
+// getExtensionImageWithTimestamp returns the extension image with the latest timestamp
+// for a given distribution and pgMajor.
+func getExtensionImageWithTimestamp(metadata *extensionMetadata, distribution string, pgMajor int) (string, error) {
+	imageName := fmt.Sprintf("ghcr.io/cloudnative-pg/%s", metadata.ImageName)
+	tags, err := crane.ListTags(imageName)
+	if err != nil {
+		return "", fmt.Errorf("while listing tags for image %s: %w", imageName, err)
+	}
+
+	version, err := extractExtensionVersion(metadata.Versions, distribution, pgMajor)
+	if err != nil {
+		return "", fmt.Errorf("while extracting extension version for %s: %w", metadata.Name, err)
+	}
+
+	re := regexp.MustCompile(
+		fmt.Sprintf(`^%s-(\d{12})-%d-%s$`,
+			regexp.QuoteMeta(version),
+			pgMajor,
+			distribution,
+		),
+	)
+
+	var latestTag string
+	for _, tag := range tags {
+		if re.MatchString(tag) && tag > latestTag {
+			latestTag = tag
+		}
+	}
+	if latestTag == "" {
+		return "", fmt.Errorf(
+			"no image found for image %s (version=%s pgMajor=%d os=%s)",
+			imageName, version, pgMajor, distribution,
+		)
+	}
+
+	imageRef := fmt.Sprintf("%s:%s", imageName, latestTag)
+
+	ref, err := name.ParseReference(imageRef, name.Insecure)
+	if err != nil {
+		return "", fmt.Errorf("while parsing image reference %s: %w", imageRef, err)
+	}
+
+	desc, err := remote.Get(ref)
+	if err != nil {
+		return "", fmt.Errorf("while fetching digest for image %s: %w", imageRef, err)
+	}
+
+	return fmt.Sprintf("%s@%s", imageRef, desc.Digest.String()), nil
+}
+
+// extractExtensionVersion returns the extension version for a given distribution and pgMajor,
+// extracted from the extension's metadata.
+func extractExtensionVersion(versions versionMap, distribution string, pgMajor int) (string, error) {
+	packageVersion := versions[distribution][strconv.Itoa(pgMajor)]
 	if packageVersion == "" {
 		return "", fmt.Errorf("no package version found for distribution %q and version %d",
-			DefaultDistribution, DefaultPgMajor)
+			distribution, pgMajor)
 	}
 
 	re := regexp.MustCompile(`^(\d+(?:\.\d+)+)`)
@@ -62,9 +137,6 @@ func getDefaultExtensionImage(metadata *extensionMetadata) (string, error) {
 	if len(matches) < 2 {
 		return "", fmt.Errorf("cannot extract extension version from %q", packageVersion)
 	}
-	version := matches[1]
-	image := fmt.Sprintf("ghcr.io/cloudnative-pg/%s:%s-%d-%s",
-		metadata.ImageName, version, DefaultPgMajor, DefaultDistribution)
 
-	return image, nil
+	return matches[1], nil
 }
